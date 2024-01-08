da7dec8f251213531df341f4088083dae414b19a
Louis <louisp_chn@icloud.com>
2024-01-08 22:07:05 +0100

    Je sais plus ce que j'ai fait, mais je suis content.

--- js/app.js
+++ js/app.js
@@ -1,24 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let backToTopButton = document.getElementById("back-to-top");
+  let backToTopButton = document.getElementById("back-to-top");
 
-    window.addEventListener("scroll", function () {
-        if (
-            document.body.scrollTop > 20 ||
-            document.documentElement.scrollTop > 20
-        ) {
-            backToTopButton.style.display = "block";
-        } else {
-            backToTopButton.style.display = "none";
-        }
-    });
+  window.addEventListener("scroll", function () {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      backToTopButton.style.display = "block";
+    } else {
+      backToTopButton.style.display = "none";
+    }
+  });
 
-    backToTopButton.addEventListener("click", function () {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-    });
+  backToTopButton.addEventListener("click", function () {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  });
 });
 window.onload = function () {
-    window.scrollTo(0, 0);
+  window.scrollTo(0, 0);
 };
 history.replaceState({}, document.title, window.location.pathname);
 
@@ -30,39 +30,39 @@ const ctx = canvas.getContext("2d");
 let mouseMoved = false;
 
 const pointer = {
-    x: 0.5 * window.innerWidth,
-    y: 0.5 * window.innerHeight,
+  x: 0.5 * window.innerWidth,
+  y: 0.5 * window.innerHeight,
 };
 const params = {
-    pointsNumber: 40,
-    widthFactor: 0.3,
-    mouseThreshold: 0.6,
-    spring: 0.4,
-    friction: 0.5,
+  pointsNumber: 40,
+  widthFactor: 0.3,
+  mouseThreshold: 0.6,
+  spring: 0.4,
+  friction: 0.5,
 };
 
 const trail = new Array(params.pointsNumber);
 for (let i = 0; i < params.pointsNumber; i++) {
-    trail[i] = {
-        x: pointer.x,
-        y: pointer.y,
-        dx: 0,
-        dy: 0,
-    };
+  trail[i] = {
+    x: pointer.x,
+    y: pointer.y,
+    dx: 0,
+    dy: 0,
+  };
 }
 
 window.addEventListener("mousemove", (e) => {
-    mouseMoved = true;
-    updateMousePosition(e.clientX, e.clientY);
+  mouseMoved = true;
+  updateMousePosition(e.clientX, e.clientY);
 });
 window.addEventListener("touchmove", (e) => {
-    mouseMoved = true;
-    updateMousePosition(e.targetTouches[0].pageX, e.targetTouches[0].pageY);
+  mouseMoved = true;
+  updateMousePosition(e.targetTouches[0].pageX, e.targetTouches[0].pageY);
 });
 
 function updateMousePosition(eX, eY) {
-    pointer.x = eX;
-    pointer.y = eY;
+  pointer.x = eX;
+  pointer.y = eY;
 }
 
 setupCanvas();
@@ -70,56 +70,84 @@ update(0);
 window.addEventListener("resize", setupCanvas);
 
 function update(t) {
-    if (!mouseMoved) {
-        pointer.x =
-            (0.5 + 0.3 * Math.cos(0.002 * t) * Math.sin(0.005 * t)) *
-            window.innerWidth;
-        pointer.y =
-            (0.5 + 0.2 * Math.cos(0.005 * t) + 0.1 * Math.cos(0.01 * t)) *
-            window.innerHeight;
-    }
+  if (!mouseMoved) {
+    pointer.x =
+      (0.5 + 0.3 * Math.cos(0.002 * t) * Math.sin(0.005 * t)) *
+      window.innerWidth;
+    pointer.y =
+      (0.5 + 0.2 * Math.cos(0.005 * t) + 0.1 * Math.cos(0.01 * t)) *
+      window.innerHeight;
+  }
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    var gradient = ctx.createRadialGradient(
-        trail[0].x,
-        trail[0].y,
-        5,
-        trail[0].x,
-        trail[0].y,
-        20
-    );
-    gradient.addColorStop(0, "white");
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  var gradient = ctx.createRadialGradient(
+    trail[0].x,
+    trail[0].y,
+    5,
+    trail[0].x,
+    trail[0].y,
+    20
+  );
+  gradient.addColorStop(0, "white");
 
-    ctx.strokeStyle = gradient;
-    trail.forEach((p, pIdx) => {
-        const prev = pIdx === 0 ? pointer : trail[pIdx - 1];
-        const spring = pIdx === 0 ? 0.4 * params.spring : params.spring;
-        p.dx += (prev.x - p.x) * spring;
-        p.dy += (prev.y - p.y) * spring;
-        p.dx *= params.friction;
-        p.dy *= params.friction;
-        p.x += p.dx;
-        p.y += p.dy;
-    });
+  ctx.strokeStyle = gradient;
+  trail.forEach((p, pIdx) => {
+    const prev = pIdx === 0 ? pointer : trail[pIdx - 1];
+    const spring = pIdx === 0 ? 0.4 * params.spring : params.spring;
+    p.dx += (prev.x - p.x) * spring;
+    p.dy += (prev.y - p.y) * spring;
+    p.dx *= params.friction;
+    p.dy *= params.friction;
+    p.x += p.dx;
+    p.y += p.dy;
+  });
 
-    ctx.lineCap = "round";
-    ctx.beginPath();
-    ctx.moveTo(trail[0].x, trail[0].y);
+  ctx.lineCap = "round";
+  ctx.beginPath();
+  ctx.moveTo(trail[0].x, trail[0].y);
 
-    for (let i = 1; i < trail.length - 1; i++) {
-        const xc = 0.5 * (trail[i].x + trail[i + 1].x);
-        const yc = 0.5 * (trail[i].y + trail[i + 1].y);
-        ctx.quadraticCurveTo(trail[i].x, trail[i].y, xc, yc);
-        ctx.lineWidth = params.widthFactor * (params.pointsNumber - i);
-        ctx.stroke();
-    }
-    ctx.lineTo(trail[trail.length - 1].x, trail[trail.length - 1].y);
+  for (let i = 1; i < trail.length - 1; i++) {
+    const xc = 0.5 * (trail[i].x + trail[i + 1].x);
+    const yc = 0.5 * (trail[i].y + trail[i + 1].y);
+    ctx.quadraticCurveTo(trail[i].x, trail[i].y, xc, yc);
+    ctx.lineWidth = params.widthFactor * (params.pointsNumber - i);
     ctx.stroke();
+  }
+  ctx.lineTo(trail[trail.length - 1].x, trail[trail.length - 1].y);
+  ctx.stroke();
 
-    window.requestAnimationFrame(update);
+  window.requestAnimationFrame(update);
 }
 
 function setupCanvas() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 }
+
+// Partie modale de formulaire de contact
+
+// Obtenir la modale
+var modal = document.getElementById("contactModal");
+
+// Obtenir le bouton qui ouvre la modale
+var btn = document.querySelector(".btn-primary");
+
+// Obtenir l'élément <span> qui ferme la modale
+var span = document.getElementsByClassName("close")[0];
+
+// Lorsque l'utilisateur clique sur le bouton, ouvrir la modale
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// Lorsque l'utilisateur clique sur <span> (x), fermer la modale
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// Lorsque l'utilisateur clique n'importe où en dehors de la modale, fermer celle-ci
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
