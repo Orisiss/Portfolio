78ae035e9a594613fdcab62c1ef3d8ee45b68bbe
Louis <louisp_chn@icloud.com>
2024-01-13 23:22:17 +0100

    test

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+import Swiper from "swiper";
+
 document.addEventListener("DOMContentLoaded", function () {
   let backToTopButton = document.getElementById("back-to-top");
 
@@ -210,4 +212,15 @@ const observer = new IntersectionObserver((entries) => {
 const projectItems = document.querySelectorAll(".project-item");
 projectItems.forEach((item) => {
   observer.observe(item);
+});
+
+// Partie navigation dans le carousel pour mobile
+
+const swiper = new Swiper('.projects', {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
