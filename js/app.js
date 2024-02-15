805bd22180eccbeb81a40cdb20650f654709bec3
Louis <louisp_chn@icloud.com>
2024-02-15 11:56:20 +0100

    Modification du système de carousel pour les projets

--- js/app.js
+++ js/app.js
@@ -30,132 +30,187 @@ window.onload = function () {
 };
 history.replaceState({}, document.title, window.location.pathname);
 
-/**
- * Code to create a trailing effect from the mouse pointer.
- * Gets canvas context, tracks mouse move and idle state.
- * Defines pointer, parameters, trail, and event listeners.
- * Draws quadratic bezier curve trail on canvas by tracking pointer.
- * Updates canvas size on window resize.
- */
-const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
+const cols = 3;
+const main = document.getElementById("main");
+let parts = [];
 
-let mouseMoved = false;
-let mouseInactiveTimer;
+let images = [
+  "https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80",
+  "https://images.unsplash.com/photo-1544198365-f5d60b6d8190?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80",
+  "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2700&q=80",
+];
+let current = 0;
+let playing = false;
 
-const pointer = {
-  x: 0.5 * window.innerWidth,
-  y: 0.5 * window.innerHeight,
+for (let i in images) {
+  new Image().src = images[i];
+}
+
+for (let col = 0; col < cols; col++) {
+  let part = document.createElement("div");
+  part.className = "part";
+  let el = document.createElement("div");
+  el.className = "section";
+  let img = document.createElement("img");
+  img.src = images[current];
+  el.appendChild(img);
+  part.style.setProperty("--x", (-100 / cols) * col + "vw");
+  part.appendChild(el);
+  main.appendChild(part);
+  parts.push(part);
+}
+
+let animOptions = {
+  duration: 2.3,
+  ease: Power4.easeInOut,
 };
-const params = {
-  pointsNumber: 40,
-  widthFactor: 0.3,
-  mouseThreshold: 0.6,
-  spring: 0.4,
-  friction: 0.5,
-};
 
-const trail = new Array(params.pointsNumber);
-for (let i = 0; i < params.pointsNumber; i++) {
-  trail[i] = {
-    x: pointer.x,
-    y: pointer.y,
-    dx: 0,
-    dy: 0,
-  };
-}
+function go(dir) {
+  if (!playing) {
+    playing = true;
+    if (current + dir < 0) current = images.length - 1;
+    else if (current + dir >= images.length) current = 0;
+    else current += dir;
 
-const links = document.querySelectorAll("a");
+    function up(part, next) {
+      part.appendChild(next);
+      gsap
+        .to(part, { ...animOptions, y: -window.innerHeight })
+        .then(function () {
+          part.children[0].remove();
+          gsap.to(part, { duration: 0, y: 0 });
+        });
+    }
 
-links.forEach((link) => {
-  link.addEventListener("mouseover", () => {
-    canvas.style.pointerEvents = "none";
-  });
+    function down(part, next) {
+      part.prepend(next);
+      gsap.to(part, { duration: 0, y: -window.innerHeight });
+      gsap.to(part, { ...animOptions, y: 0 }).then(function () {
+        part.children[1].remove();
+        playing = false;
+      });
+    }
 
-  link.addEventListener("mouseout", () => {
-    canvas.style.pointerEvents = "all";
-  });
-});
+    for (let p in parts) {
+      let part = parts[p];
+      let next = document.createElement("div");
+      next.className = "section";
+      let img = document.createElement("img");
+      img.src = images[current];
+      next.appendChild(img);
 
-window.addEventListener("mousemove", (e) => {
-  mouseMoved = true;
-  updateMousePosition(e.clientX, e.clientY);
-});
-window.addEventListener("touchmove", (e) => {
-  mouseMoved = true;
-  updateMousePosition(e.targetTouches[0].pageX, e.targetTouches[0].pageY);
-});
-
-document.addEventListener("mousemove", function () {
-  clearTimeout(mouseInactiveTimer);
-  mouseInactiveTimer = setTimeout(function () {
-    mouseMoved = false;
-  }, 10000);
-});
-
-function updateMousePosition(eX, eY) {
-  pointer.x = eX;
-  pointer.y = eY;
-}
-
-setupCanvas();
-update(0);
-window.addEventListener("resize", setupCanvas);
-
-function update(t) {
-  if (!mouseMoved) {
-    pointer.x =
-      (0.5 + 0.3 * Math.cos(0.002 * t) * Math.sin(0.005 * t)) *
-      window.innerWidth;
-    pointer.y =
-      (0.5 + 0.2 * Math.cos(0.005 * t) + 0.1 * Math.cos(0.01 * t)) *
-      window.innerHeight;
+      if ((p - Math.max(0, dir)) % 2) {
+        down(part, next);
+      } else {
+        up(part, next);
+      }
+    }
   }
+}
 
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  var gradient = ctx.createRadialGradient(
-    trail[0].x,
-    trail[0].y,
-    5,
-    trail[0].x,
-    trail[0].y,
-    20
-  );
-  gradient.addColorStop(0, "white");
-
-  ctx.strokeStyle = gradient;
-  trail.forEach((p, pIdx) => {
-    const prev = pIdx === 0 ? pointer : trail[pIdx - 1];
-    const spring = pIdx === 0 ? 0.4 * params.spring : params.spring;
-    p.dx += (prev.x - p.x) * spring;
-    p.dy += (prev.y - p.y) * spring;
-    p.dx *= params.friction;
-    p.dy *= params.friction;
-    p.x += p.dx;
-    p.y += p.dy;
-  });
-
-  ctx.lineCap = "round";
-  ctx.beginPath();
-  ctx.moveTo(trail[0].x, trail[0].y);
-
-  for (let i = 1; i < trail.length - 1; i++) {
-    const xc = 0.5 * (trail[i].x + trail[i + 1].x);
-    const yc = 0.5 * (trail[i].y + trail[i + 1].y);
-    ctx.quadraticCurveTo(trail[i].x, trail[i].y, xc, yc);
-    ctx.lineWidth = params.widthFactor * (params.pointsNumber - i);
-    ctx.stroke();
+window.addEventListener("keydown", function (e) {
+  if (["ArrowDown", "ArrowRight"].includes(e.key)) {
+    go(1);
+  } else if (["ArrowUp", "ArrowLeft"].includes(e.key)) {
+    go(-1);
   }
-  ctx.lineTo(trail[trail.length - 1].x, trail[trail.length - 1].y);
-  ctx.stroke();
+});
 
-  window.requestAnimationFrame(update);
+function lerp(start, end, amount) {
+  return (1 - amount) * start + amount * end;
 }
 
-function setupCanvas() {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+const cursor = document.createElement("div");
+cursor.className = "cursor";
+
+const cursorF = document.createElement("div");
+cursorF.className = "cursor-f";
+let cursorX = 0;
+let cursorY = 0;
+let pageX = 0;
+let pageY = 0;
+let size = 8;
+let sizeF = 36;
+let followSpeed = 0.16;
+
+document.body.appendChild(cursor);
+document.body.appendChild(cursorF);
+
+if ("ontouchstart" in window) {
+  cursor.style.display = "none";
+  cursorF.style.display = "none";
 }
+
+cursor.style.setProperty("--size", size + "px");
+cursorF.style.setProperty("--size", sizeF + "px");
+
+window.addEventListener("mousemove", function (e) {
+  pageX = e.clientX;
+  pageY = e.clientY;
+  cursor.style.left = e.clientX - size / 2 + "px";
+  cursor.style.top = e.clientY - size / 2 + "px";
+});
+
+function loop() {
+  cursorX = lerp(cursorX, pageX, followSpeed);
+  cursorY = lerp(cursorY, pageY, followSpeed);
+  cursorF.style.top = cursorY - sizeF / 2 + "px";
+  cursorF.style.left = cursorX - sizeF / 2 + "px";
+  requestAnimationFrame(loop);
+}
+
+loop();
+
+let startY;
+let endY;
+let clicked = false;
+
+function mousedown(e) {
+  gsap.to(cursor, { scale: 4.5 });
+  gsap.to(cursorF, { scale: 0.4 });
+
+  clicked = true;
+  startY = e.clientY || e.touches[0].clientY || e.targetTouches[0].clientY;
+}
+function mouseup(e) {
+  gsap.to(cursor, { scale: 1 });
+  gsap.to(cursorF, { scale: 1 });
+
+  endY = e.clientY || endY;
+  if (clicked && startY && Math.abs(startY - endY) >= 40) {
+    go(!Math.min(0, startY - endY) ? 1 : -1);
+    clicked = false;
+    startY = null;
+    endY = null;
+  }
+}
+window.addEventListener("mousedown", mousedown, false);
+window.addEventListener("touchstart", mousedown, false);
+window.addEventListener(
+  "touchmove",
+  function (e) {
+    if (clicked) {
+      endY = e.touches[0].clientY || e.targetTouches[0].clientY;
+    }
+  },
+  false
+);
+window.addEventListener("touchend", mouseup, false);
+window.addEventListener("mouseup", mouseup, false);
+
+let scrollTimeout;
+function wheel(e) {
+  clearTimeout(scrollTimeout);
+  setTimeout(function () {
+    if (e.deltaY < -40) {
+      go(-1);
+    } else if (e.deltaY >= 40) {
+      go(1);
+    }
+  });
+}
+window.addEventListener("mousewheel", wheel, false);
+window.addEventListener("wheel", wheel, false);
 
 /**
  * Handles horizontal mouse scrolling for the carousel element.
