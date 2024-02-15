f03ab2784ad998d12db442bd215894ff0737ff8e
Louis <louisp_chn@icloud.com>
2024-02-15 10:51:01 +0100

    On enleve le background qui ne fonctionne pas

--- js/app.js
+++ js/app.js
@@ -90,7 +90,7 @@ document.addEventListener("mousemove", function () {
   clearTimeout(mouseInactiveTimer);
   mouseInactiveTimer = setTimeout(function () {
     mouseMoved = false;
-  }, 5000);
+  }, 10000);
 });
 
 function updateMousePosition(eX, eY) {
@@ -156,78 +156,6 @@ function setupCanvas() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 }
-
-/**
- * Defines constants, class, and functions to create bouncing blob elements on the page.
- * MIN_SPEED and MAX_SPEED define the range of random velocity values.
- * randomNumber() generates a random number within a min and max range.
- * Blob class handles the position, velocity, and element updates for each blob.
- * initBlobs() finds all .bouncing-blob elements, initializes Blob instances,
- * and starts the animation update loop.
- */
-const MIN_SPEED = 1.5;
-const MAX_SPEED = 2.5;
-function randomNumber(min, max) {
-  return Math.random() * (max - min) + min;
-}
-class Blob {
-  constructor(el) {
-    this.el = el;
-    const boundingRect = this.el.getBoundingClientRect();
-    this.size = boundingRect.width;
-    this.initialX = randomNumber(0, window.innerWidth - this.size);
-    this.initialY = randomNumber(0, window.innerHeight - this.size);
-    this.el.style.top = `${this.initialY}px`;
-    this.el.style.left = `${this.initialX}px`;
-    this.vx =
-      randomNumber(MIN_SPEED, MAX_SPEED) * (Math.random() > 0.5 ? 1 : -1);
-    this.vy =
-      randomNumber(MIN_SPEED, MAX_SPEED) * (Math.random() > 0.5 ? 1 : -1);
-    this.x = this.initialX;
-    this.y = this.initialY;
-  }
-  update() {
-    this.x += this.vx;
-    this.y += this.vy;
-    if (this.x >= window.innerWidth - this.size) {
-      this.x = window.innerWidth - this.size;
-      this.vx *= -1;
-    }
-    if (this.y >= window.innerHeight - this.size) {
-      this.y = window.innerHeight - this.size;
-      this.vy *= -1;
-    }
-    if (this.x <= 0) {
-      this.x = 0;
-      this.vx *= -1;
-    }
-    if (this.y <= 0) {
-      this.y = 0;
-      this.vy *= -1;
-    }
-  }
-  move() {
-    this.el.style.transform = `translate(${this.x - this.initialX}px, ${
-      this.y - this.initialY
-    }px)`;
-  }
-}
-
-function initBlobs() {
-  const blobEls = document.querySelectorAll(".bouncing-blob");
-  const blobs = Array.from(blobEls).map((blobEl) => new Blob(blobEl));
-
-  function update() {
-    requestAnimationFrame(update);
-    blobs.forEach((blob) => {
-      blob.update();
-      blob.move();
-    });
-  }
-  requestAnimationFrame(update);
-}
-
-initBlobs();
 
 /**
  * Handles horizontal mouse scrolling for the carousel element.
